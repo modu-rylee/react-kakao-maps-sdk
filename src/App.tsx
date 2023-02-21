3a8aa3f2884cf78c075d8a0816211e80120d7f2f
rylee <rylee@moduparking.com>
2023-02-21 12:47:35 +0900

    feat: mapData type 지정, payload로 사용할 값 정리

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,60 @@ function App() {
     // 지도의 크기
     const style = {
         width: "500px",
-        height: "450px",
+        height: "280px",
     }
 
-    const [deg, setDeg] = useState(
+    const [viewPoint, setViewPoint] = useState(
         {
-            pan: 0, // 좌우각도
-            tilt: 0 // 위아래각도
+            pan: 0, // 좌우각도 / min: 0 / max: 360
+            tilt: 0, // 위아래각도 / min: -85 / max: 85
+            zoom: 0 // 확대,축소 / min: -3 / max: 3
         }
     )
 
     const onViewpointChange = (roadview: any) => {
-        console.log(roadview.getViewpoint())
-        setDeg({pan: roadview.getViewpoint().pan, tilt: roadview.getViewpoint().tile})
+        const {pan, tilt, zoom} = roadview.getViewpoint()
+        console.log(pan, tilt,zoom)
+        setViewPoint({ pan, tilt, zoom})
+    }
+    const onPositionChanged = (roadview: any) => {
+        const {La, Ma} =roadview.getPosition()
+    }
+
+    interface MapData {
+        // pan과 tilt는 소수점 6자리까지 잘라서 전송
+        lat: number, // 위도, 소수
+        lng: number, // 경도, 소수
+        pan: number, //  좌우각도, 소수 / min: 0 / max: 360
+        tilt: number, // 위아래각도, 소수 / min: -85 / max: 85
+        zoom: number // 확대축소, 정수 / min: -3 / max: 3
+        address: string // 주소
     }
 
     return (
         <div className="App">
             <Roadview // 로드뷰를 표시할 Container
+                onViewpointChange={onViewpointChange}
+                onPositionChanged={onPositionChanged}
                 position={position}
                 style={style}
-                onViewpointChange={onViewpointChange}
+                pan={34.59694259544006}
+                tilt={0.21984650896155908}
+                zoom={0}
+            />
+            <Roadview
+                position={position}
+                style={style}
+                pan={34.5969425}
+                tilt={0.2198465}
+            />
+
+            <Roadview
+                position={position}
+                style={style}
+                pan={34}
+                tilt={0}
+                zoom={0}
             />
         </div>
   )
